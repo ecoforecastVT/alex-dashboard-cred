72750d9e58fe6c5fb5e13e540fa94302c64fb3c7
addelany <addelany@gmail.com>
2025-02-07 16:09:36 -0600

    fix text and add widgit functions

---
+++ R/climatology_calc.R
@@ -0,0 +1,27 @@
+
+climatology_calc <- function(obs_df, day_of_interest, clim_var){
+  
+  current_df <- obs_df |> 
+    filter(datetime == day_of_interest,
+           variable == clim_var) |> 
+    mutate(doy = lubridate::yday(as.Date(datetime)))
+  
+  clim_avg <- obs_df |> 
+    mutate(doy = lubridate::yday(as.Date(datetime))) |> 
+    filter(datetime < day_of_interest, 
+           variable == clim_var, 
+           doy == current_df$doy) |> 
+    summarize(obs_avg = mean(observation, na.rm = TRUE),
+              obs_95 = quantile(observation, 0.95,),
+              obs_05 = quantile(observation, 0.05))
+  
+  if ((current_df$observation <= clim_avg$obs_95) & (current_df$observation >= clim_avg$obs_05)){
+    arrow_icon <- 'arrow-bar-right'
+  } else if (current_df$observation > clim_avg$obs_95){
+    arrow_icon <- 'arrow-up-square'
+  } else if (current_df$observation < clim_avg$obs_05){
+    arrow_icon <- 'arrow-down-square'
+  }
+
+  return(list(clim_avg$obs_avg, arrow_icon))
+}
