75f94fdc7507d3a513700afe84ddbc6c706c2980
addelany <addelany@gmail.com>
2025-02-21 15:28:56 -0600

    add historic and future trend function

--- R/historic_trend_calc.R
+++ R/historic_trend_calc.R
@@ -1,29 +1,27 @@
-future_trend_calc <- function(day_of_interest, interest_var, days_ahead, interest_site){
+historic_trend_calc <- function(day_of_interest, interest_var, days_historic, interest_site){
   
   s3_score <- arrow::s3_bucket(bucket = "bio230121-bucket01/flare/scores/parquet", 
                                endpoint_override = "renc.osn.xsede.org", 
                                anonymous = TRUE)
   
-  if (interest_var == 'depth'){
-  score_df <- arrow::open_dataset(s3_score) |>
-    filter(variable == interest_var,
-           site_id %in% interest_site,
-           horizon >= 0,
-           horizon <= days_ahead,
-           reference_datetime == (day_of_interest),
-           model_id == 'glm_flare_v3') |>
-    dplyr::collect() |> 
-    mutate(rownum = row_number())
+  if(interest_var == 'depth'){
+    score_df <- arrow::open_dataset(s3_score) |>
+      filter(variable == interest_var,
+             site_id %in% interest_site,
+             horizon == 0,
+             model_id == 'glm_flare_v3') |>
+      dplyr::collect() |> 
+      filter(datetime >= (day_of_interest - lubridate::days(days_historic))) |> 
+      mutate(rownum = row_number())
   } else {
     score_df <- arrow::open_dataset(s3_score) |>
       filter(variable == interest_var,
              site_id %in% interest_site,
+             horizon == 0,
              depth %in% c(0.5),
-             horizon >= 0,
-             horizon <= days_ahead,
-             reference_datetime == (day_of_interest),
              model_id == 'glm_flare_v3') |>
       dplyr::collect() |> 
+      filter(datetime >= (day_of_interest - lubridate::days(days_historic))) |> 
       mutate(rownum = row_number())
   }
   ## FIGURE OUT THE CRITERIA WE WANT TO USE FOR THE TREND SIGNIFICANCE
@@ -33,7 +31,7 @@ future_trend_calc <- function(day_of_interest, interest_var, days_ahead, interes
   trend_model <- lm(score_df$mean ~ score_df$rownum)
   var_trend <- trend_model$coefficients[[2]]
   
-  var_threshold <- mean(score_df$mean, na.rm = TRUE)*0.05
+  var_threshold <- mean(score_df$mean)*0.05
   
   if ((var_trend <= var_threshold) & (var_trend >= var_threshold)){
     arrow_icon <- 'arrow-bar-right'
@@ -42,6 +40,9 @@ future_trend_calc <- function(day_of_interest, interest_var, days_ahead, interes
   } else if (var_trend < var_threshold){
     arrow_icon <- 'arrow-down-square'
   }
+  
+  return(list(round(var_trend,1), arrow_icon))
+ 
   
   return(list(round(var_trend,1), arrow_icon))
 }
